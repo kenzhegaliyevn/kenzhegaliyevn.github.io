3a9f6830d6797a33c9e65c8b00ad20438c2092d2
knursultan <knursultana@gmail.com>
2017-12-17 13:53:17 +0600

    fix

--- sunqar/js/common.js
+++ sunqar/js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	$(".menu_icon").click(function(){
+	$(".menu_icon svg").click(function(){
 		console.log("click");
-		// $('.main_menu').slideToggle();
+		$('.main_menu').slideToggle();
 	});
 });
