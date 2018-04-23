cfe2c0c077b07a61be8491cf6a9a2508f560ecf9
knursultan <knursultana@gmail.com>
2018-04-23 13:38:36 +0600

    phone mask

--- order/js/common.js
+++ order/js/common.js
@@ -18,33 +18,82 @@ $(document).ready(function() {
         });
     }).call(this);
 
-	$(".header_content_left a").bind('click', function(){
-		$(".popup").addClass("is_open");
-		$(document.body).addClass("not_scroll");
-	});
+    $(".header_content_left a").bind('click', function() {
+        $(".popup").addClass("is_open");
+        $(document.body).addClass("not_scroll");
+    });
 
-	$(".popup_close").bind('click', function(){
-		$(".popup").removeClass("is_open");
-		$(document.body).removeClass("not_scroll");		
-	});
+    $(".popup_close").bind('click', function() {
+        $(".popup").removeClass("is_open");
+        $(document.body).removeClass("not_scroll");
+    });
 
     document.addEventListener('keydown', e => {
         if (e.keyCode === 27) {
-			$(".popup").removeClass("is_open");
-			$(document.body).removeClass("not_scroll");
+            $(".popup").removeClass("is_open");
+            $(document.body).removeClass("not_scroll");
         }
     });
 
-    $(".popup_content input[type='submit']").click(function(){
-		$(".popup_content form").addClass("hide");
-		$(".thanks_block").removeClass("hide");
+    $(".popup_content input[type='submit']").click(function() {
+        $(".popup_content form").addClass("hide");
+        $(".thanks_block").removeClass("hide");
     });
 
 
-    $("a.scrollTo").click(function(){
+    $("a.scrollTo").click(function() {
         var elementClick = $(this).attr("href");
         var destination = $(elementClick).offset().top;
-        $("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 1000);
+        $("html:not(:animated), body:not(:animated)").animate({ scrollTop: destination }, 1000);
         return false;
     });
+
+    $('.phone-number')
+        .keydown(function(e) {
+            var key = e.which || e.charCode || e.keyCode || 0;
+            $phone = $(this);
+
+            if ($phone.val().length === 1 && (key === 8 || key === 46)) {
+                $phone.val('(');
+                return false;
+            }
+            else if ($phone.val().charAt(0) !== '(') {
+                $phone.val('(' + String.fromCharCode(e.keyCode) + '');
+            }
+            if (key !== 8 && key !== 9) {
+                if ($phone.val().length === 4) {
+                    $phone.val($phone.val() + ')');
+                }
+                if ($phone.val().length === 5) {
+                    $phone.val($phone.val() + ' ');
+                }
+                if ($phone.val().length === 9) {
+                    $phone.val($phone.val() + '-');
+                }
+            }
+            return (key == 8 ||
+                key == 9 ||
+                key == 46 ||
+                (key >= 48 && key <= 57) ||
+                (key >= 96 && key <= 105));
+        })
+
+        .bind('focus click', function() {
+            $phone = $(this);
+
+            if ($phone.val().length === 0) {
+                $phone.val('(');
+            } else {
+                var val = $phone.val();
+                $phone.val('').val(val);
+            }
+        })
+
+        .blur(function() {
+            $phone = $(this);
+
+            if ($phone.val() === '(') {
+                $phone.val('');
+            }
+        });
 });
