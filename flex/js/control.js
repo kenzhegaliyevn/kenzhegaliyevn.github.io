991ace1df82d0db75bdffad1fad6021778b95d65
knursultan <knursultana@gmail.com>
2018-06-08 13:13:20 +0600

    js

---
+++ flex/js/control.js
@@ -0,0 +1,25 @@
+// JavaScript document
+
+$(function () {
+    'use strict';
+
+    $('a[href^="#"]').click(function () {
+        let target = $(this).attr('href');
+        $('html, body').animate(
+            {
+                scrollTop: $(target).offset().top
+            },
+            800
+        );
+
+        var offset = $('#bottom_header').offset();
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > offset.top) {
+                $('#bottom_header').addClass('fixedNav');
+            } else {
+                $('#bottom_header').removeClass('fixedNav');
+            }
+        });
+    });
+});
