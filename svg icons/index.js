d2f05f1684309f4c805219303658da033df90adf
Нурсултан <nursultan@MacBook-Air-Nursultan.local>
2018-10-11 15:22:51 +0600

    svg logos

---
+++ svg icons/index.js
@@ -0,0 +1,72 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+});
+
+var animation1 = bodymovin.loadAnimation({
+    container: document.getElementById('bm-1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-1.json'
+});
+
+var animation2 = bodymovin.loadAnimation({
+    container: document.getElementById('bm-2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-2.json'
+});
+
+var animation3 = bodymovin.loadAnimation({
+    container: document.getElementById('bm-3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-3.json'
+});
+
+var animation4 = bodymovin.loadAnimation({
+    container: document.getElementById('bm-4'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-4.json'
+});
+
+var animation5 = bodymovin.loadAnimation({
+    container: document.getElementById('bm-5'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-5.json'
+});
+
+var animation6 = bodymovin.loadAnimation({
+    container: document.getElementById('bm-6'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-6.json'
+});
+
+var animation7 = bodymovin.loadAnimation({
+    container: document.getElementById('bm-7'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-7.json'
+});
+
+var animation8 = bodymovin.loadAnimation({
+    container: document.getElementById('bm-8'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-8.json'
+});
+
