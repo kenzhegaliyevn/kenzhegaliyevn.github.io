73236ddc43aa62660255ecb5103e1d9457d5d508
knursultan <knursultana@gmail.com>
2020-04-06 02:04:32 +0500

    build

---
+++ build/precache-manifest.c760af0addc315a18c39cbbc301840a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186c1ab489951dcbf8944670e1ee5b08",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "abafe75e5142c8e1f480",
+    "url": "/quiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a01bfa12761104d151c3",
+    "url": "/quiz/static/js/2.b490e052.chunk.js"
+  },
+  {
+    "revision": "fd90558609d7fb1dba29d57d85830b8f",
+    "url": "/quiz/static/js/2.b490e052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abafe75e5142c8e1f480",
+    "url": "/quiz/static/js/main.99ea1c66.chunk.js"
+  },
+  {
+    "revision": "6ff49e0f8b4d9414dd0f",
+    "url": "/quiz/static/js/runtime-main.1a44d9f9.js"
+  }
+]);
